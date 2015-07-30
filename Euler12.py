ca23aafc47854feaa5b15a58fc309a2bc6fae0e3
JulianCienfuegos <melvyniandrag@gmail.com>
2015-07-29 22:10:47 -0400

    numba 12

--- Euler12.py
+++ Euler12.py
@@ -1,37 +1,37 @@
-# What is the first triangle number to have over five hundred divisors?
-# for this we wil use dynamic programming
+def prime_factorization(n):
+	# count the number of divisors of a number n.
+	# we can count the prime divisors, and then use the formula (n_1 + 1)*...*(n_N + 1) to get the total number of factors.
+	d = {}
+	i = 2
+	while i < (n + 1):
+		if n%i==0:
+			repeat = 1
+			while repeat == 1:
+				try:
+					d[i] += 1
+				except:
+					d[i] = 1
+				n = n/i
+				if n == 1:
+					return d
+				if n%i != 0:
+					repeat = 0
+		i+=1
+	return d
 
-def triGen():
-    total = 0
-    for i in range(100000000000):
-        total = i + total
-        yield total
+def count_divisors(d):
+	# Accept the dictionary 
+	divs = 1
+	for k in d:
+		divs *= (d[k] + 1)
+	return divs
+		
 
-def countDivisors(N):
-    n_divisors = 0
-    for n in range(1, N//2  + 1):
-        if N%n == 0:
-            n_divisors += 1
-    return n_divisors
-
-tri = triGen()
-
-def printTri(num):
-    for j in range(num):
-        l=[]
-        for i in range(10):
-            l.append(next(tri))
-        print(l)
-printTri(10)
-
-
-"""
-tri = triGen()
-n_divisors = 0
-while n_divisors < 500:
-    N = next(tri)
-    n_divisors = countDivisors(N)
-    print(N, n_divisors)
-
-print(N)
-"""
+triangle = 0
+i = 1
+divs = 0
+while divs < 501:
+	triangle += i
+	divs = count_divisors(prime_factorization(triangle))
+	i += 1
+print divs, triangle
